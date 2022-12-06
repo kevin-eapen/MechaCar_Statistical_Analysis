81d182424835333305ebb0dffec057794c423730
Kevin Eapen <kevineapen777@gmail.com>
2022-12-05 23:24:56 -0800

    Completed Deliverable 3 technical analysis.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(tidyverse)
 
 
 # Deliverable 1 - Technical Analysis
@@ -15,7 +16,10 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 susp_coil_table = read.csv(file="Resources/Suspension_Coil.csv", check.name=F, stringsAsFactors=F)
 
 
+
+
 # Deliverable 2 - Technical Analysis
+
 # Create summary dataframe for mean, median, variance, and standard deviation of susp_coil PSI column
 total_summary <- susp_coil_table %>% summarize(Mean=mean(PSI), Median=median(PSI),
                                         Variance=var(PSI), SD=sd(PSI))
@@ -26,4 +30,18 @@ lot_summary <- susp_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mea
                                                                              Median=median(PSI),
                                                                              Variance=var(PSI), 
                                                                              sd(PSI))
+
+# Deliverable 3 - Technical Analysis
+
+# One-sample t-test comparing sample to population mean of PSI=1500
+t.test(susp_coil_table$PSI, mu=1500)
+
+# One-sample t-test comparing Lot1 sample subset to population mean of PSI=1500
+t.test(formula=PSI~1, data=susp_coil_table, mu=1500, subset=Manufacturing_Lot=="Lot1")
+
+# One-sample t-test comparing Lot2 sample subset to population mean of PSI=1500
+t.test(formula=PSI~1, data=susp_coil_table, mu=1500, subset=Manufacturing_Lot=="Lot2")
+
+# One-sample t-test comparing Lot3 sample subset to population mean of PSI=1500
+t.test(formula=PSI~1, data=susp_coil_table, mu=1500, subset=Manufacturing_Lot=="Lot3")
 
